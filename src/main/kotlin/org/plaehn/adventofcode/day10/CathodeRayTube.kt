7ab90536dac98107698b5a3f261016901b7480fd
Oliver Plaehn <Plaehn.Oliver@swm.de>
2022-12-17 13:14:11 +0100

    Day 10: Refactor

--- src/main/kotlin/org/plaehn/adventofcode/day10/CathodeRayTube.kt
+++ src/main/kotlin/org/plaehn/adventofcode/day10/CathodeRayTube.kt
@@ -3,39 +3,42 @@ package org.plaehn.adventofcode.day10
 
 class CathodeRayTube(private val instructions: List<Instruction>) {
 
-    fun drawCrtImage(): String {
-        var image = ""
+    fun drawCrtImage(): String =
+        processInstructions("") { cycle, x, image ->
+            var newImage = image
+            newImage += if ((cycle - 1) % 40 in (x - 1..x + 1)) '#' else '.'
+            if (cycle % 40 == 0) {
+                newImage += "\n"
+            }
+            newImage
+        }.trim()
+
+    fun computeSumOfSignalStrength(): Int =
+        processInstructions(0) { cycle, x, sum ->
+            sum + if (cycle.isInterestingSignal()) {
+                cycle * x
+            } else {
+                0
+            }
+        }
+
+    private fun <T> processInstructions(
+        initialValue: T,
+        processCycle: (Int, Int, T) -> T
+    ): T {
+        var output: T = initialValue
         var x = 1
         var cycle = 0
 
         instructions.forEach { instruction ->
             repeat(instruction.cycles) {
-                image += if (cycle % 40 in (x - 1..x + 1)) '#' else '.'
                 ++cycle
-                if (cycle % 40 == 0) {
-                    image += "\n"
-                }
+                output = processCycle(cycle, x, output)
             }
             x += instruction.amount
         }
-        return image.trim()
+        return output
     }
-
-    fun computeSumOfSignalStrength() =
-        sequence {
-            var x = 1
-            var cycle = 0
-
-            instructions.forEach { instruction ->
-                repeat(instruction.cycles) {
-                    ++cycle
-                    if (cycle.isInterestingSignal()) {
-                        yield(cycle * x)
-                    }
-                }
-                x += instruction.amount
-            }
-        }.sum()
 
     private fun Int.isInterestingSignal() = this in listOf(20, 60, 100, 140, 180, 220)
 
