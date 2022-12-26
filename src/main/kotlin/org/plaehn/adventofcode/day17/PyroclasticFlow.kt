e22dfda148a498cece9260db406b04de374a458d
Oliver Plaehn <Plaehn.Oliver@swm.de>
2022-12-26 13:24:27 +0100

    Day 17, part 1

--- src/main/kotlin/org/plaehn/adventofcode/day17/PyroclasticFlow.kt
+++ src/main/kotlin/org/plaehn/adventofcode/day17/PyroclasticFlow.kt
@@ -1,19 +1,89 @@
 package org.plaehn.adventofcode.day17
 
+import org.plaehn.adventofcode.common.Coord
 import org.plaehn.adventofcode.common.Matrix
+import org.plaehn.adventofcode.common.cycle
 import org.plaehn.adventofcode.common.groupByBlankLines
-import org.plaehn.adventofcode.day17.Element.EMPTY
+import org.plaehn.adventofcode.day17.Element.*
 
 
 class PyroclasticFlow(val jetPattern: List<Direction>, val shapes: List<Matrix<Element>>) {
 
-    fun computeTowerHeight(numberOfRocks: Int): Int {
-        println(jetPattern.joinToString(""))
-        println(shapes.joinToString("\n\n"))
-        TODO()
+    private val chamber = createChamber()
+    private val directions = jetPattern.asSequence().cycle().iterator()
+    private val fallingRocks = shapes.asSequence().cycle().iterator()
+
+
+    private fun createChamber(): Matrix<Element> {
+        val chamber = Matrix.fromDimensions(WIDTH, HEIGHT, EMPTY)
+        chamber.drawFloor()
+        chamber.drawWalls()
+        return chamber
     }
 
+    private fun Matrix<Element>.drawFloor() {
+        Coord(1, 0).lineTo(Coord(WIDTH - 2, 0)).forEach {
+            this[it] = FLOOR
+        }
+    }
+
+    private fun Matrix<Element>.drawWalls() {
+        Coord(0, 0).lineTo(Coord(0, HEIGHT - 1)).forEach {
+            this[it] = WALL
+        }
+        Coord(WIDTH - 1, 0).lineTo(Coord(WIDTH - 1, HEIGHT - 1)).forEach {
+            this[it] = WALL
+        }
+    }
+
+    fun computeTowerHeight(numberOfRocks: Int): Int {
+        repeat(numberOfRocks) {
+            drop(fallingRocks.next())
+        }
+
+        return chamber.towerHeight()
+    }
+
+    private fun drop(rock: Matrix<Element>) {
+        var position = Coord(3, chamber.towerHeight() + 4)
+
+        do {
+            var newPosition = position + directions.next().offset
+            if (!overlap(chamber, rock, newPosition)) {
+                position = newPosition
+            }
+
+            newPosition = position + Coord(0, -1)
+            if (overlap(chamber, rock, newPosition)) {
+                break
+            }
+            position = newPosition
+        } while (true)
+
+        addToChamber(rock, position)
+    }
+
+    private fun overlap(chamber: Matrix<Element>, rock: Matrix<Element>, position: Coord) =
+        rock.toMap().any { (coord, rockElement) ->
+            chamber[coord + position] != EMPTY && rockElement != EMPTY
+        }
+
+    private fun addToChamber(rock: Matrix<Element>, position: Coord) {
+        rock.toMap().forEach { (coord, rockElement) ->
+            if (rockElement == ROCK) {
+                chamber[coord + position] = rockElement
+            }
+        }
+    }
+
+    private fun <T> Matrix<T>.towerHeight() = rows().drop(1).takeWhile { !it.isFree() }.size
+
+    private fun <E> List<E>.isFree() = drop(1).dropLast(1).all { it == EMPTY }
+
     companion object {
+        private const val WIDTH = 9
+        private const val HEIGHT = 10000
+
         fun fromInput(jetPattern: String, shapes: String) =
             PyroclasticFlow(
                 jetPattern = jetPattern.map { Direction.fromInput(it) },
@@ -21,19 +91,22 @@ class PyroclasticFlow(val jetPattern: List<Direction>, val shapes: List<Matrix<E
             )
 
         private fun String.toShape(): Matrix<Element> =
-            Matrix.fromRows(lines().map { line -> line.map { Element.fromInput(it) } }, EMPTY)
+            Matrix.fromRows(lines().reversed().map { line -> line.map { Element.fromInput(it) } }, EMPTY)
     }
 }
 
-
 enum class Element {
     ROCK,
-    EMPTY;
+    EMPTY,
+    WALL,
+    FLOOR;
 
     override fun toString() =
         when (this) {
             ROCK -> "#"
             EMPTY -> "."
+            WALL -> "|"
+            FLOOR -> "-"
         }
 
     companion object {
@@ -46,9 +119,10 @@ enum class Element {
     }
 }
 
-enum class Direction {
-    LEFT,
-    RIGHT;
+enum class Direction(val offset: Coord) {
+
+    LEFT(Coord(-1, 0)),
+    RIGHT(Coord(1, 0));
 
     override fun toString() =
         when (this) {
--- src/test/kotlin/org/plaehn/adventofcode/day17/PyroclasticFlowTest.kt
+++ src/test/kotlin/org/plaehn/adventofcode/day17/PyroclasticFlowTest.kt
@@ -8,7 +8,7 @@ import org.plaehn.adventofcode.common.slurp
 internal class PyroclasticFlowTest {
 
     @Test
-    fun `Compute maximum pressure release for test input`() {
+    fun `Compute tower height for test input`() {
         val jetPattern = this::class.java.slurp("test_input.txt")
         val shapes = this::class.java.slurp("rock_shapes.txt")
 
@@ -16,5 +16,16 @@ internal class PyroclasticFlowTest {
         val height = flow.computeTowerHeight(numberOfRocks = 2022)
 
         assertThat(height).isEqualTo(3068)
+    }
+
+    @Test
+    fun `Compute tower height for puzzle input`() {
+        val jetPattern = this::class.java.slurp("puzzle_input.txt")
+        val shapes = this::class.java.slurp("rock_shapes.txt")
+
+        val flow = PyroclasticFlow.fromInput(jetPattern, shapes)
+        val height = flow.computeTowerHeight(numberOfRocks = 2022)
+
+        assertThat(height).isEqualTo(3147)
     }
 }
