e0516754fc283264b5c3d1ad5d87d2e30a7c22f0
Oliver Plaehn <Plaehn.Oliver@swm.de>
2022-12-31 17:40:51 +0100

    Day 18, part 2

--- src/main/kotlin/org/plaehn/adventofcode/day18/BoilingBoulders.kt
+++ src/main/kotlin/org/plaehn/adventofcode/day18/BoilingBoulders.kt
@@ -3,14 +3,42 @@ package org.plaehn.adventofcode.day18
 import org.plaehn.adventofcode.common.Coord
 
 
-class BoilingBoulders(val cubes: List<Coord>) {
+class BoilingBoulders(val cubes: Set<Coord>) {
 
-    fun computeSurfaceArea() = cubes.sumOf { 6 - it.countNeighbors() }
+    fun computeSurfaceArea() = cubes.sumOf { 6 - it.countCubeNeighbors() }
 
-    private fun Coord.countNeighbors() = neighbors(dimensions = 3).count { it in cubes }
+    fun computeSurfaceAreaWithoutAirPockets() = computeSurfaceArea() - computeAirPocketSurfaceArea()
+
+    private fun computeAirPocketSurfaceArea(): Int {
+        val areaSpanned = sequence {
+            (cubes.minOf { it.x }..cubes.maxOf { it.x }).forEach { x ->
+                (cubes.minOf { it.y }..cubes.maxOf { it.y }).forEach { y ->
+                    (cubes.minOf { it.z }..cubes.maxOf { it.z }).forEach { z ->
+                        yield(Coord(x, y, z))
+                    }
+                }
+            }
+        }.toSet()
+
+        val reachable = mutableSetOf<Coord>()
+        do {
+            val newReachable = areaSpanned.filter { coord ->
+                coord !in reachable && coord !in cubes && coord.neighbors(dimensions = 3).any { neighbor ->
+                    neighbor in reachable || neighbor !in areaSpanned
+                }
+            }.toSet()
+            reachable += newReachable
+        } while (newReachable.isNotEmpty())
+
+        return areaSpanned
+            .filter { coord -> coord !in cubes && coord !in reachable }
+            .sumOf { coord -> coord.countCubeNeighbors() }
+    }
+
+    private fun Coord.countCubeNeighbors() = neighbors(dimensions = 3).count { it in cubes }
 
     companion object {
         fun fromInput(input: List<String>) =
-            BoilingBoulders(cubes = input.map { Coord.fromString(it) })
+            BoilingBoulders(cubes = input.map { Coord.fromString(it) }.toSet())
     }
 }
--- src/test/kotlin/org/plaehn/adventofcode/day18/BoilingBouldersTest.kt
+++ src/test/kotlin/org/plaehn/adventofcode/day18/BoilingBouldersTest.kt
@@ -7,7 +7,6 @@ import org.plaehn.adventofcode.common.readLines
 
 internal class BoilingBouldersTest {
 
-
     @Test
     fun `Compute surface area for test input`() {
         val input = this::class.java.readLines("test_input.txt")
@@ -26,5 +25,25 @@ internal class BoilingBouldersTest {
         val area = boilingBoulders.computeSurfaceArea()
 
         assertThat(area).isEqualTo(4536)
+    }
+
+    @Test
+    fun `Compute surface area without air pockets for test input`() {
+        val input = this::class.java.readLines("test_input.txt")
+
+        val boilingBoulders = BoilingBoulders.fromInput(input)
+        val area = boilingBoulders.computeSurfaceAreaWithoutAirPockets()
+
+        assertThat(area).isEqualTo(58)
+    }
+
+    @Test
+    fun `Compute surface area without air pockets for puzzle input`() {
+        val input = this::class.java.readLines("puzzle_input.txt")
+
+        val boilingBoulders = BoilingBoulders.fromInput(input)
+        val area = boilingBoulders.computeSurfaceAreaWithoutAirPockets()
+
+        assertThat(area).isEqualTo(2606)
     }
 }
