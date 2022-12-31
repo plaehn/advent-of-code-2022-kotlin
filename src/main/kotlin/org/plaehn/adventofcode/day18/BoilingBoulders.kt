fe44f4d3a17ab684c10652fd61a7b5612c422a56
Oliver Plaehn <Plaehn.Oliver@swm.de>
2022-12-31 17:56:01 +0100

    Day 18: refactorings

--- src/main/kotlin/org/plaehn/adventofcode/day18/BoilingBoulders.kt
+++ src/main/kotlin/org/plaehn/adventofcode/day18/BoilingBoulders.kt
@@ -7,35 +7,46 @@ class BoilingBoulders(val cubes: Set<Coord>) {
 
     fun computeSurfaceArea() = cubes.sumOf { 6 - it.countCubeNeighbors() }
 
+    private fun Coord.countCubeNeighbors() = neighbors(dimensions = 3).count { it in cubes }
+
     fun computeSurfaceAreaWithoutAirPockets() = computeSurfaceArea() - computeAirPocketSurfaceArea()
 
     private fun computeAirPocketSurfaceArea(): Int {
-        val areaSpanned = sequence {
+        val areaSpanned = computeAreaSpannedByCubes()
+        val reachable = computeReachableSpots(areaSpanned)
+        val spotsInAirPockets = areaSpanned.filter { it !in cubes && it !in reachable }
+        return spotsInAirPockets.sumOf { it.countCubeNeighbors() }
+    }
+
+    private fun computeAreaSpannedByCubes() =
+        sequence {
             (cubes.minOf { it.x }..cubes.maxOf { it.x }).forEach { x ->
                 (cubes.minOf { it.y }..cubes.maxOf { it.y }).forEach { y ->
                     (cubes.minOf { it.z }..cubes.maxOf { it.z }).forEach { z ->
-                        yield(Coord(x, y, z))
+                        this.yield(Coord(x, y, z))
                     }
                 }
             }
         }.toSet()
 
+    private fun computeReachableSpots(areaSpanned: Set<Coord>): MutableSet<Coord> {
         val reachable = mutableSetOf<Coord>()
         do {
-            val newReachable = areaSpanned.filter { coord ->
-                coord !in reachable && coord !in cubes && coord.neighbors(dimensions = 3).any { neighbor ->
-                    neighbor in reachable || neighbor !in areaSpanned
-                }
-            }.toSet()
+            val newReachable = computeNewReachableSpots(areaSpanned, reachable)
             reachable += newReachable
         } while (newReachable.isNotEmpty())
-
-        return areaSpanned
-            .filter { coord -> coord !in cubes && coord !in reachable }
-            .sumOf { coord -> coord.countCubeNeighbors() }
+        return reachable
     }
 
-    private fun Coord.countCubeNeighbors() = neighbors(dimensions = 3).count { it in cubes }
+    private fun computeNewReachableSpots(areaSpanned: Set<Coord>, reachable: MutableSet<Coord>) =
+        areaSpanned.filter { spot ->
+            spot !in reachable
+                && spot !in cubes
+                && spot.hasReachableNeighbor(reachable, areaSpanned)
+        }.toSet()
+
+    private fun Coord.hasReachableNeighbor(reachable: Set<Coord>, areaSpanned: Set<Coord>) =
+        neighbors(dimensions = 3).any { neighbor -> neighbor in reachable || neighbor !in areaSpanned }
 
     companion object {
         fun fromInput(input: List<String>) =
